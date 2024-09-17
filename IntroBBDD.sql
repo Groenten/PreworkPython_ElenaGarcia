a587630f40ea759964a320c657540f9a99b54936
Groenten <e.garciaverduras@gmail.com>
2024-09-17 12:20:40 +0200

    Update IntroBBDD.sql

---
+++ IntroBBDD.sql
@@ -0,0 +1,14 @@
+SELECT flight_id, flight_no 
+FROM Flights
+WHERE status = 'On Time'
+SELECT *
+FROM Bookings
+WHERE total_amount > 1000000
+SELECT *
+FROM Aircrafts
+SELECT flight_id
+FROM Flights
+WHERE aircraft_code = 733
+SELECT *
+FROM Tickets
+WHERE passenger_name = 'Irina'
